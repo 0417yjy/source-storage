18987cb8e64fd35d0bcc4bd6ba961b0d97aff5e4
Jongyeon Yoon <0417yjy@naver.com>
2020-08-20 14:30:20 +0900

    Add coding test template for one line with integer input

---
+++ C++/Template/coding_test_template_oneline_int.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+/* Write your own Classes, Structures, or Functions here */
+
+/* Main Function */
+int main(void) {
+    int input;
+    int answer;
+    // string answer;
+
+    /* Get input */
+    cin >> input;
+
+    /* Write your solution here */
+
+    /* Print outputs here */
+    cout << answer;
+
+    return 0;
+}
