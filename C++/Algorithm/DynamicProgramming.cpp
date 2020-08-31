e0468c97bbc2ff9b17027c8596b6350c8a9fc559
Jongyeon Yoon <0417yjy@naver.com>
2020-08-31 12:26:28 +0900

    DP의 방식에 따라 클래스를 분리. 누적적으로 계산되는 것은 DP 클래스에서 상속받는 것으로 변경

--- C++/Algorithm/DynamicProgramming.cpp
+++ C++/Algorithm/DynamicProgramming.cpp
@@ -4,7 +4,6 @@ DynamicProgramming::DynamicProgramming(int size, std::function<void(int*, int)> 
     // initialize member values
     this->size = size;
     this->calculatingFcn = fun;
-    calculated_acc_idx = -1;
     arr = new int[size];
 }
 
@@ -12,7 +11,6 @@ DynamicProgramming::DynamicProgramming(int size, std::function<void(int*, int)> 
     // initialize member values
     this->size = size;
     this->calculatingFcn = fun;
-    calculated_acc_idx = -1;
     arr = new int[size];
 
     initialize(initializer, init_size);
@@ -23,14 +21,47 @@ void DynamicProgramming::initialize(int* initializer, int size) {
         size = this->size;
     }
 
-    for(int i=0;i<size;i++) {
+    for(int i=0;i<size;i++) { // 주어진 배열만큼 초기화
         arr[i] = initializer[i];
     }
 
-    calculated_acc_idx = size - 1;
+    for(int i=size;i<this->size;i++) { // 나머지는 -1로 채움
+        arr[i] = -1;
+    }
 }
 
 int DynamicProgramming::get(int idx) {
+    if(idx >= size || idx < 0) {
+        return -1;
+    }
+
+    if(arr[idx] != -1) {
+        return arr[idx];
+    }
+    else {
+        calculatingFcn(arr, idx);
+        return arr[idx];    
+    }
+}
+
+DynamicProgramming::~DynamicProgramming() {
+    delete[] arr;
+}
+
+// ---------------------------------------------------------------------------------------------------------------------
+
+DynamicProgrammingAcc::DynamicProgrammingAcc(int size, std::function<void(int*, int)> fun) : DynamicProgramming(size, fun) {
+    calculated_acc_idx = -1;
+}
+
+DynamicProgrammingAcc::DynamicProgrammingAcc(int size, std::function<void(int*, int)> fun, int *initializer, int init_size) : DynamicProgramming(size, fun, initializer, init_size) {
+    calculated_acc_idx = -1;
+}
+
+int DynamicProgrammingAcc::get(int idx) {
+    if(idx >= size || idx < 0) {
+        return -1;
+    }
     if(idx <= calculated_acc_idx) {
         return arr[idx];
     }
@@ -41,8 +72,4 @@ int DynamicProgramming::get(int idx) {
         calculated_acc_idx = idx;
         return arr[idx];
     }
-}
-
-DynamicProgramming::~DynamicProgramming() {
-    delete[] arr;
 }
--- C++/Algorithm/DynamicProgramming.h
+++ C++/Algorithm/DynamicProgramming.h
@@ -1,17 +1,16 @@
 // DP 에 범용적으로 쓸 클래스
 #include <functional>
 
-class DynamicProgramming {
+class DynamicProgramming { // DP 베이스 클래스
   public:
     DynamicProgramming(int size, std::function<void(int*, int)> fun); // 기본 생성자
     DynamicProgramming(int size, std::function<void(int*, int)> fun, int *initializer, int init_size); // 이니셜라이저를 포함한 생성자
     void initialize(int* initializer, int size); // DP 배열의 초기화
     int get(int idx); // 특정 인덱스의 값을 가져오기
-  private:
+  protected:
     ~DynamicProgramming(); // 소멸자
     std::function<void(int*, int)> calculatingFcn; // 배열에 값을 저장하는 규칙 함수
     int size; // 배열의 크기
-    int calculated_acc_idx; // 0부터 계산이 완료된 인덱스
     int *arr; // 배열 포인터
 };
 
@@ -24,3 +23,13 @@ void dp_function(int* arr, int idx) {
 
 과 같은 형식으로 작성하면 됨
 */
+
+class DynamicProgrammingAcc : public DynamicProgramming {
+  // 배열의 값을 누적으로 채울 때 사용 (특정 인덱스의 값을 계산할 때 그 전까지의 모든 인덱스들의 값이 계산되어 있어야 할 경우)
+  public:
+    DynamicProgrammingAcc(int size, std::function<void(int*, int)> fun); // 기본 생성자
+    DynamicProgrammingAcc(int size, std::function<void(int*, int)> fun, int *initializer, int init_size); // 이니셜라이저를 포함한 생성자
+    int get(int idx);
+  private:
+    int calculated_acc_idx; // 0부터 계산이 완료된 인덱스
+};
