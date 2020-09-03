1bbe9258e3f77479f44eb2d934563128f8fbf193
Jongyeon Yoon <0417yjy@naver.com>
2020-09-03 17:19:02 +0900

    C 전용 코딩테스트 템플릿 추가

---
+++ C/Template/coding_test_template_oneline_int.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/* Write your own Structures or Functions here */
+
+/* Main Function */
+int main(void) {
+    int input;
+    int answer;
+    // string answer;
+
+    /* Get input */
+    scanf("%d", &input);
+
+    /* Write your solution here */
+
+    /* Print outputs here */
+    printf("%d", answer);
+
+    return 0;
+}
